dc43d5bd5a4bd7d8012f078922640c38d7024d3b
Jan du Plessis <drjanduplessis@icloud.com>
2024-01-02 02:56:48 +0000

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content]
 
 setup(
-    name='XeroConvert',
+    name='xeroconvert',
     description="App to convert PCSE Incoices to Xero Import format.",
     packages=find_packages(),  # It will find all packages in your directory
     install_requires=requirements, # This is the key line to install dependencies
